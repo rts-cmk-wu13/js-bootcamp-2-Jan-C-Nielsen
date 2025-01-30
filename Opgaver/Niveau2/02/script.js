c6b749c937f849ab6e536cfb2d4effffcac1309e
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-01-30 09:31:04 +0100

    only one function

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -2,27 +2,22 @@ let addbuttons = document.querySelectorAll(".product__add");
 let subbuttons = document.querySelectorAll(".product__sub");
 let products = Array.from(document.querySelectorAll(".product"));
 console.log(products);
-products.forEach(function(x) {
+products.forEach(function (x) {
     console.log(x);
-    
-    x.querySelector(".product__add").addEventListener("click", add);
-    x.querySelector(".product__sub").addEventListener("click", sub);  
+    x.querySelector(".product__add").addEventListener("click", calc);
+    x.querySelector(".product__sub").addEventListener("click", calc);
 })
 
-let numbers = [1,1,1];
+let numbers = [1, 1, 1];
 
-function add(e) {
+function calc(e) {
+    console.log(e.target.getAttribute("class"));
     let number = e.currentTarget.parentNode.parentNode.querySelector(".product__number");
     let parent = e.currentTarget.parentNode.parentNode.parentNode;
     let index = products.indexOf(parent);
-    numbers[index]++;
-    number.innerHTML = numbers[index];
-}
-
-function sub(e) {
-    let number = e.currentTarget.parentNode.parentNode.querySelector(".product__number");
-    let parent = e.currentTarget.parentNode.parentNode.parentNode;
-    let index = products.indexOf(parent);
-    numbers[index] = numbers[index] > 0 ? numbers[index]-1 : 0;
+    if (e.target.getAttribute("class") === "product__add")
+        numbers[index]++;
+    else
+        numbers[index] = numbers[index] > 0 ? numbers[index] - 1 : 0;
     number.innerHTML = numbers[index];
 }
