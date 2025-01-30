e8b02be65ae60f20d2d0b9e75e4e331cc9083dc1
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-01-30 08:36:29 +0100

    3 vare og sved på panden

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -1,19 +1,34 @@
-let addbutton = document.querySelector(".add");
-let subbutton = document.querySelector(".sub");
+let addbuttons = document.querySelectorAll(".add");
+let subbuttons = document.querySelectorAll(".sub");
+let products = Array.from(document.querySelectorAll(".product"));
+console.log(products);
+products.forEach(function(x) {
+    console.log(x);
+    
+    x.querySelector(".add").addEventListener("click", add);
+    x.querySelector(".sub").addEventListener("click", sub);  
+})
 
-addbutton.addEventListener("click", add);
-subbutton.addEventListener("click", sub);
+let numbers = [1,1,1];
 
-let price = 0;
-
-function add() {
-    console.log("add");
-    price++;
-    document.querySelector(".number").innerHTML = price;
+function add(e) {
+    let number = e.currentTarget.parentNode.parentNode.querySelector(".number");
+    console.log(number);
+    let parent = e.currentTarget.parentNode.parentNode.parentNode;
+    let index = products.indexOf(parent);
+    console.log( numbers[products.indexOf(parent)]);
+    numbers[index]++;
+    console.log(numbers);
+    number.innerHTML = numbers[index] ;
 }
 
-function sub() {
-    console.log("sub");
-    price = price > 0 ? price-1 : 0;
-    document.querySelector(".number").innerHTML = price;
+function sub(e) {
+    let number = e.currentTarget.parentNode.parentNode.querySelector(".number");
+    console.log(number);
+    let parent = e.currentTarget.parentNode.parentNode.parentNode;
+    let index = products.indexOf(parent);
+    console.log( numbers[products.indexOf(parent)]);
+    numbers[index] = numbers[index] > 0 ? numbers[index]-1 : 0;
+    console.log(numbers);
+    number.innerHTML = numbers[index] ;
 }
